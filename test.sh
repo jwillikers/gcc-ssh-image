2bb34a5eceb46b9167ae5cef59f07a04e9749acd
Jordan Williams <jordan@jwillikers.com>
2021-06-24 16:09:30 -0500

    Fix syntax of test commands

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
-podman run --rm --name test-container -dt -p 2222:22 localhost/gcc-ssh
+podman run --userns keep-id --volume "$PWD":/home/user:Z --rm --name test-container -dt -p 2222:22 localhost/gcc-ssh
 sleep 5
 nc -z localhost 22
 
-podman exec --userns keep-id --user user --rm --volume "$PWD":/home/user:Z --name test-container localhost/gcc-ssh g++ test/main.cpp
-podman exec --userns keep-id --user user --rm --volume "$PWD":/home/user:Z --name test-container localhost/gcc-ssh ./a.out
+podman exec --user user --rm test-container g++ test/main.cpp
+podman exec --user user --rm test-container ./a.out
 rm a.out
 
 podman stop test-container
