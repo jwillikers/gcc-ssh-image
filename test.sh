a013ae3078b3049f57f4f88cf9e8d2b8363a2233
Jordan Williams <jordan@jwillikers.com>
2021-06-25 16:27:54 -0500

    Simplify testing in CI

--- test.sh
+++ test.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-podman run --userns keep-id --volume "$PWD":/home/user:Z --rm --name test-container -dt -p 2222:22 localhost/gcc-ssh
+podman run --rm --name test-container -dt -p 2222:22 localhost/gcc-ssh
 sleep 5
 nc -z localhost 22
-
-podman exec --user user test-container g++ test/main.cpp
-podman exec --user user test-container ./a.out
-rm a.out
 
 podman stop test-container
